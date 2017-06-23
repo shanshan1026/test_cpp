553a437695355ece559e49edd02e125f6b518dbe
shanshan1026 <pengss1026@gmail.com>
2017-06-23 15:23:19 +0800

    check in test_cons.cpp

---
+++ test_cons.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class CBase {
+public:
+    CBase(){print();}
+    virtual void print(){cout << "I am Base" << endl;}
+};
+class CDerived: public CBase {
+public:
+    CDerived(){print();}
+    virtual void print(){cout << "I am Derived" << endl;}
+} ;
+ 
+int main()
+{
+    cout << "Base construct" << endl;
+    CBase b;
+    cout << "Derived construct" << endl;
+    CDerived d;
+    CBase* pb;
+    CDerived* pd;
+ 
+    cout << "Base pointer" << endl;
+    pb = &b;   
+    pb->print();
+    pb = &d;    
+    pb->print();
+
+} 
+
